65a90891b372ad94df796a7bbdf6fe7a1b43221f
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-07-08 17:38:35 -0300

    Destroy every profile files and folders

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
   root 'main/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
